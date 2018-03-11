4399f1c76f62927acb282db807151e3c110a12eb
DarkwinXXX <victor.m.m@gmail.com>
2018-03-11 12:47:57 -0300

    Actualización

--- MiniLidar-Escaner.ino
+++ MiniLidar-Escaner.ino
@@ -7,109 +7,51 @@
 #define DELAY_BETWEEN_SAMPLES 100
 
 // Tamaño de los pasos de YAW / PITCH en grados (1 = resolución completa)
-#define YAW_STEP 2
-#define PITCH_STEP 2
+#define PITCH_STEP 1
 #define MATH_PI 3.1415f
+#define GRAD2RAD 0.0174533f
 
 //Variables
+PasosStepper = 200; //modificar si se usa micropasos
+AngPasos = PasosStepper / 360f; //calculo grados por cada paso= nº de pasos que dará el motor dividido por 360º
+
 LIDARLite myLidarLite;
-Servo servoYaw;
-Servo servoPitch;
+r = myLidarLite.distance();
+//theta es el ángulo de giro en X
+theta = (float)AngPasos acumulado por cada paso!; 
+//theta = (float)reemplazo * PI / 360.0f;
+//phi es al ángulo de giro entre Y y Z
+phi = (float)AngPasos acumulado por cada paso!;
+//phi = (float)pitchAngle * PI / 160.0f;
 
-//variable pololu
-const int dirPin = 8;
-const int stepPin = 9;
+//Conversión de grados a radianes
+thetarad = (float)theta * GRAD2RAD;
+phirad = (float)phi * GRAD2RAD;
 
-//variables
-const int steps = 200;
-int microPausa = 10000;
-char s[15];
-int yawAngle;
-int pitchAngle;
-int x;
-int y;
-int z;
-int r;
-int rojo = 2 ;
-int azul = 5;
-int verde = 7;
-
-int boton = 6;
-
-float theta;
-float phi;
-int reemplazo;
-boolean bandera = false;
-File myFile;
-
-void setup() {
-  // Inicialice la conexión en serie para visualizar las lecturas de distancia
-  Serial.begin(115200);
-
-  Serial.println("inicializacion exitosa");
-
-  pinMode(dirPin, OUTPUT);
-  pinMode(stepPin, OUTPUT);
-
-  pinMode(verde, OUTPUT);
-  pinMode(azul, OUTPUT);
-  pinMode(rojo, OUTPUT);
-
-  pinMode(boton, INPUT_PULLUP);
-
-  digitalWrite(rojo, HIGH);
-
-  //Servo init
-  servoPitch.attach(3);
-  
-  //Lidar Lite v3 init
-  myLidarLite.begin(0, true);
-  myLidarLite.configure(0);
-
-  if (!SD.begin(4)) {
-    return;
-  }
-}
-
-void loop() {
-  if (digitalRead(boton) == LOW) {
-    bandera = true;
-    sensar();
-  } else {
-    Serial.println("stand By");
-  }
-}
-
-//Función para adquirir, convertir y enviar la medición.
-void sendMeasurement() {
-  digitalWrite(azul, HIGH);
-  myFile = SD.open("log.txt", FILE_WRITE);
-  delay(DELAY_BETWEEN_SAMPLES);
-  digitalWrite(azul, LOW);
-  //Obtener coordenadas esféricas
-  r = myLidarLite.distance(false);
-  theta = (float)reemplazo * PI / 360.0f;
-  phi = (float)pitchAngle * PI / 180.0f;
-
-  // Convertir y enviarlos
-  sprintf(s, "%d,%d,%d\n\0", (int)(r * cos(phi)*cos(theta)), (int)(r * cos(phi)*sin(theta)), (int)(r * sin(phi)));
-  Serial.print(s);
-  myFile.println(s);
-  myFile.close();
+// Calcular y enviarlos
+sprintf(s, "%d,%d,%d\n\0", (int)(r * cos(phiard)*cos(thetarad)), (int)(r * cos(phirad)*sin(thetarad)), (int)(r * sin(phirad)));
+ 
+Serial.print(s);
+Serial.print(" - ");
+Serial.println(myLidarLite.distance()); 
+ 
+myFile.println(s);
+myFile.close();
 }
 
 void sensar() {
-  myFile = SD.open("xyz.txt", FILE_WRITE);
+  myFile = SD.open("log.txt", FILE_WRITE);
   delay(5000);// espera
 
   if (myFile && bandera == true) {
 
     digitalWrite(rojo, LOW);
-    //Barrido Yaw paso a passo
-    for (int b = 0; b <= steps; b++) {
+    //Barrido Yaw servomotor
+    for (int b = 0; b <= PasosStepper; b++) {
       digitalWrite(dirPin, HIGH);//determinas direccion
       reemplazo = b;
       reemplazo = map(reemplazo, 0, 200, 0, 360);
+
       digitalWrite(stepPin, HIGH);
       delayMicroseconds(microPausa);
       digitalWrite(stepPin, LOW);
